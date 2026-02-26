24c0a70b2de9405c72c6992bba37264fbebcd0ca
Kusu Priyanka <kusupriyanka04@gmail.com>
2026-02-26 17:48:32 +0530

    Update StarRating.js

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -8,7 +8,7 @@ export default function StarRating({ rating }) {
         if (i < fullStars) {
           return <span key={i} className="text-yellow-400">★</span>;
         } else if (i === fullStars && halfStar) {
-          return <span key={i} className="text-yellow-400">☆</span>;
+          return <span key={i} className="text-yellow-400" style={{opacity: 0.5}}>☆</span>;
         } else {
           return <span key={i} className="text-gray-300">★</span>;
         }
@@ -17,3 +17,4 @@ export default function StarRating({ rating }) {
     </div>
   );
 }
+
